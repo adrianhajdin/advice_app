be84792dc7a4a33e80e13a255eeea55c7e16c170
adrianhajdin <adrianhajdin00@gmail.com>
2020-03-08 09:32:36 +0100

    change

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
